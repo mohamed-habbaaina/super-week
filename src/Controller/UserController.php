2adc42ab389521452379bab82fa8492969188e59
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-05-07 16:50:59 +0200

    PHP, UserController, method: isValid, validName,VALIDeMAIL, VALIDpASSWORD

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Controller;
+require_once('./../Model/UserModel.php');
+
 use App\Model\UserModel;
 class UserController
 {
@@ -14,6 +16,56 @@ class UserController
     {
         $data = $this->user->findAll();
         return json_encode($data);
+    }
+
+    public function isValid(string $input): string
+    {
+
+        $input = trim($input);
+        $input = htmlspecialchars($input);
+        $input = strip_tags($input);
+
+        return $input;
+    }
+    // validate name: alphabetical letter, lower case, upper case, special letter (ç,é,,etc) and space.
+    public function validName(string $input): bool
+    {
+        $regexName = '/^[a-zA-ZÀ-ÿ\s]+$/';
+        
+        if (preg_match($regexName, $input))
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public function validEmail(string $email): bool
+    {
+        $regExpEmail = '/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-z]{2,5})$/'; 
+
+        if(preg_match($regExpEmail, $email))
+        {
+            return true;
+        } else
+        {
+            return false; 
+        }
+    }
+
+    // regExp valid Password: strlen >4, accept only [A-Za-z0-9] && minimum 1 lower case 1 upper case 1 number.
+    public function validPassword(string $password): bool
+    {
+        $regExpPass = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{4,}$/';
+
+        if(preg_match($regExpPass,$password))
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 
 }
