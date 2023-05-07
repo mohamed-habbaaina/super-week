0afb2494ee881b88039ad8d988cbfd36d52b99dc
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-05-07 20:38:02 +0200

    HTML, view register

---
+++ src/View/register.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="./src/View/js/register.js"></script>
+    <title>Register | super-week</title>
+</head>
+<body>
+    <main>
+        <div class="messageErr"></div>
+        <form action="./js/register.js" id="formRegister">
+            <label for="email">Email</label>
+            <input type="email" name="email" placeholder="Entrer Votre Email">
+            <small></small>
+
+            <label for="first_name">Prénom</label>
+            <input type="text" name="first_name" placeholder="Entrer Votre nom">
+
+            <label for="last_name">Nom</label>
+            <input type="text" name="last_name" placeholder="Entrer Votre prénom">
+
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="Entrer Votre password">
+            <small></small>
+
+            <label for="c_pass">Confirme Password</label>
+            <input type="password" name="c_pass" placeholder="Confermer Votre password">
+            <small></small>
+
+            <input type="submit" value="Valid">
+
+        </form>
+    </main>
+    
+</body>
+</html>
