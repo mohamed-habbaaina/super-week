91125301f94a3bf6d9424354e7d08bf119fb2251
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-05-08 06:33:14 +0200

    HTML, view login

---
+++ src/View/login.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+if(isset($_SESSION['user']))
+{
+    $email = $_SESSION['user']['email'];
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="./src/View/js/login.js"></script>
+    <title>Login | super-week</title>
+</head>
+<body>
+    <main>
+        <div class="messageErr"></div>
+
+        <h1>Login</h1>
+        
+        <form action="./js/login.js" id="formLogin">
+
+            <label for="email">Email</label>
+            <input type="email" name="email" 
+            <?php
+            if(isset($email))
+            {
+                echo 'value="' .$email . '">';
+                } else
+                {?>
+            placeholder="Entrer Votre Email"><?php } ?>
+
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="Entrer Votre password">
+
+            <input type="submit" value="Login">
+
+        </form>
+    </main>
+</body>
+</html>
