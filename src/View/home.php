2b948a29e741ace2497a14f5f264a7758d4ffe91
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-05-13 16:31:34 +0200

    home view

--- src/View/home.php
+++ src/View/home.php
@@ -1,6 +1,6 @@
 <?php
-$idUser = isset($_GET['idUser']) ?  $_GET['idUser']: 1;
-$idBook = isset($_GET['idBook']) ?  $_GET['idBook']: 1;
+// $idUser = isset($_GET['idUser']) ?  $_GET['idUser']: 1;
+// $idBook = isset($_GET['idBook']) ?  $_GET['idBook']: 1;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,24 +8,31 @@ $idBook = isset($_GET['idBook']) ?  $_GET['idBook']: 1;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/View/style/style.css">
     <script defer src="./src/View/js/user.js"></script>
     <title>Home | super-week</title>
 </head>
 <body>
+    <header>
+        <ul>
+            <li><button class="btn"><a href="./register">Register</a></button></li>
+            <li><button class="btn"><a href="./login">Login</a></button></li>
+        </ul>
+    </header>
     <main>
         <ul>
-            <li><button><a href="./users">Users</a></button></li>
-            <li><button><a href="./books">Books</a></button></li>
+            <li><button class="btn"><a href="./users">Users</a></button></li>
+            <li><button class="btn"><a href="./books">Books</a></button></li>
             <li>
                 <form action="./src/View/js/user.js" id="formUser">
                     <input type="number" name="user">
-                    <input type="submit" value="User">
+                    <input class="btn" type="submit" value="User">
                 </form>
             </li>
             <li>
                 <form action="./src/View/js/user.js" id="formBook">
                     <input type="number" name="book">
-                    <input type="submit" value="Book">
+                    <input class="btn" type="submit" value="Book">
                 </form>
             </li>
         </ul>
