bc72aa20126850d6e2c7db322018ad15f97503ed
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-05-14 11:03:00 +0200

    HTML, books view

---
+++ src/View/books.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/View/style/style.css">
+    <script defer src="src/View/js/books.js"></script>
+    <title>Books | super-week</title>
+</head>
+<body>
+    <main>
+        <div class="containerBooks"></div>
+    </main>
+</body>
+</html>
